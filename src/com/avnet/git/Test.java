94c9b046c142da3261737ee38725902a0c5ba032
907914 <907914@CNMAC01NB0021.APAC.AVNET.COM>
2013-12-10 10:29:38 +0800

    Signed-off-by: 907914 <907914@CNMAC01NB0021.APAC.AVNET.COM>

--- src/com/avnet/git/Test.java
+++ src/com/avnet/git/Test.java
@@ -1,10 +1,12 @@
 package com.avnet.git;
 
+import java.util.Date;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(new Date());
 	}
 
 }
